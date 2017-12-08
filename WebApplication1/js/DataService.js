6c276dd8983a7b365f40bdd1e449a5fed4cb3606
Rutkay Bozkurt <rutkaybozkurt@users.noreply.github.com>
2017-12-08 19:28:11 +0200

    Angular for Index

---
+++ WebApplication1/js/DataService.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('DataService', DataService);
+
+    DataService.$inject = ['$http'];
+
+    function DataService($http) {
+
+
+        var sendSearch = function (input)
+        {
+            var formData = new FormData();
+            console.log('Hello!');
+            formData.append('postData', JSON.stringify(input));
+
+            return $http({
+                method: 'POST',
+                url: '/CustomFormHandler.ashx?op=search',
+                data: formData,
+                //transformRequest: angular.identity,
+                headers: {
+                    'Content-Type': undefined
+                }
+
+
+            }).then(function (response) {
+                console.log('Search successful!');
+            }, function (response) {
+                console.log("Search failed!");
+            });
+        }
+        return {
+            search: function (input) {
+                return sendSearch(input);
+            }
+        };
+
+        
+    }
+
+})();
---
+++ WebApplication1/js/Home/index.controller.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('IndexController', IndexController);
+
+    IndexController.$inject = ['$location','$scope','DataService']; 
+
+    function IndexController($location,$scope, DataService) {
+        /* jshint validthis:true */
+        $scope.title = 'controller';
+
+        $scope.name = "";
+
+        $scope.search = function(input){
+            DataService.search(input);
+        }
+
+        activate();
+
+        function activate() { }
+    }
+})();
