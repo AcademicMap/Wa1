880d0df021846c387d4dfc5ef907acc085a47c69
Rutkay Bozkurt <rutkaybozkurt@users.noreply.github.com>
2017-12-10 18:03:34 +0200

    GoJs ilerleme

--- WebApplication1/js/Home/result.controller.js
+++ WebApplication1/js/Home/result.controller.js
@@ -17,6 +17,21 @@
 
         }
 
+        $scope.model = new go.GraphLinksModel(
+        [
+          { key: 1, name: "Alpha", color: "lightblue" },
+          { key: 2, name: "Beta", color: "orange" },
+          { key: 3, name: "Gamma", color: "lightgreen" },
+          { key: 4, name: "Delta", color: "pink" }
+        ],
+        [
+          { from: 1, to: 2 },
+          { from: 1, to: 3 },
+          { from: 2, to: 2 },
+          { from: 3, to: 4 },
+          { from: 4, to: 1 }
+        ]);
+        $scope.model.selectedNodeData = null;
 
         activate();
 
